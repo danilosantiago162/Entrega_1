a9bc7e2155fe10fb7562d2b40af84d74bae9fb80
Danilo Santiago Baptista <danilo.santiago.20241@poli.ufrj.br>
2024-05-29 19:02:32 -0300

    mensagem pré intereng

--- entrega1.js
+++ entrega1.js
@@ -2,53 +2,57 @@ class sistema{
     //mudança de novo
     var Lista_de_pedidos = new Array(0)
     class Pedidos{
-        constructor(ID_unico,ID_cliente,status,data_pedido){
+        constructor(ID_unico,ID_cliente,status,data_pedido) {
 
             this.ID_unico=ID_unico;
             this.ID_cliente=ID_cliente;
             this.status=status;
-            this.data_pedido=data_pedido
+            this.data_pedido=data_pedido;
         }
     }
     class Funcionário{
-        constructor(ID_unico,nome_usuario,cpf,email,senha){
+        constructor(ID_unico,nome_usuario,cpf,email,senha) {
             this.ID_unico=ID_unico;
             this.nome_usuario=nome_usuario;
             this.cpf=cpf;
             this.email=email;
-            this.senha=senha
+            this.senha=senha;
         }
     }
     class Funcionário_logado extends Funcionário {
         ver_meus_dados() {
-            console.log(ID_unico);
-            console.log(nome_usuario);
-            console.log(cpf);
-            console.log(email);
-            console.log(senha)
+            console.log("Seu ID único é:",this.ID_unico);
+            console.log("Seu nome de usuário é:",this.nome_usuario);
+            console.log("Seu CPF é:", this.cpf);
+            console.log("Seu email é:", this.email);
+            console.log("Sua senha é", this.senha);
         }
         modificar_meus_dados() {
             var requisição = require("readline-sync");
-            var número = requisição.questionInt("Se desejas alterar, \n seu nome de usuário digite 1 \n seu cpf digite 2 \n seu email digite 3 \n sua senha digite 4");
+            var número = requisição.question("Se desejas alterar, \n seu nome de usuario digite 1 \n seu cpf digite 2 \n seu email digite 3 \n sua senha digite 4");
             if (número == 1) {
                 var requisição = require("readline-sync");
-                var novonome = requisição.question("Insira novo nome de usuário desejado");
+                var novonome = requisição.question("Insira novo nome de usuario desejado\n");
                 ID_unico.nome_usuario = novonome
+                console.log("Mudado com sucesso para: ", novocpf);
             }
             if (número == 2) {
                 var requisição = require("readline-sync");
-                var novocpf = requisição.questionInt("Insira novo cpf sem traços");
+                var novocpf = requisição.question("Insira novo cpf sem traços\n");
                 ID_unico.cpf = novocpf
+                console.log("Mudado com sucesso para: ", novocpf);
             }
             if (número == 3) {
                 var requisição = require("readline-sync");
-                var novoemail = requisição.question("Insira novo email desejado");
+                var novoemail = requisição.question("Insira novo email desejado\n");
                 ID_unico.email = novoemail
+                console.log("Mudado com sucesso para: ", novocpf);
             }
             if (número == 4) {
                 var requisição = require("readline-sync");
-                var novasenha = requisição.question("Insira nova senha desejada");
+                var novasenha = requisição.question("Insira nova senha desejada\n");
                 ID_unico.senha = novasenha
+                console.log("Mudado com sucesso para: ", novocpf);
             }
         }
         ver_lista_de_pedidos() {
@@ -60,7 +64,7 @@ class sistema{
             var requisição = require("readline-sync");
             var nome = requisição.question("Qual nome de usuário você deseja?\n");
             var requisição = require("readline-sync");
-            var CPF = requisição.questionInt("Qual seu cpf (insira sem traços)?\n");
+            var CPF = requisição.question("Qual seu cpf (insira sem traços)?\n");
             var requisição = require("readline-sync");
             var EMAIL = requisição.question("Qual seu email?\n");
             var requisição = require("readline-sync");
@@ -68,13 +72,13 @@ class sistema{
             function getRandom() {
                 return Math.random();
             }
-
-            var MeusDados = new Object();
-            MeusDados.ID_unico = getRandom();
-            MeusDados.nome_usuario = nome;
-            MeusDados.cpf = CPF;
-            MeusDados.email = EMAIL;
-            MeusDados.senha = SENHA;
+        
+            var nome = new Object();
+            nome.ID_unico = getRandom();
+            nome.nome_usuario = nome;
+            nome.cpf = CPF;
+            nome.email = EMAIL;
+            nome.senha = SENHA;
         }
         Fazer_Login() {
             var requisição = require("readline-sync")
@@ -109,20 +113,20 @@ class sistema{
         }
         modificar_meus_dados() {
             var requisição = require("readline-sync");
-            var número = requisição.questionInt("Se desejas alterar, \n seu nome digite 1, \n sua data de nascimento digite 2 \n seu cpf digite 3 \n seu email digite 4 \n sua senha digite 5");
+            var número = requisição.question("Se desejas alterar, \n seu nome digite 1, \n sua data de nascimento digite 2 \n seu cpf digite 3 \n seu email digite 4 \n sua senha digite 5");
             if (número == 1) {
                 var requisição = require("readline-sync");
-                var novonomec = requisição.prompt("Insira seu novo nome");
+                var novonomec = requisição.question("Insira seu novo nome");
                 ID_unico.nome_cliente = novonomec
             }
             if (número == 2) {
                 var requisição = require("readline-sync")
-                var novadata = requisição.prompt("Insira nova data de nascimento");
+                var novadata = requisição.question("Insira nova data de nascimento");
                 ID_unico.data_de_nascimento = novadata
             }
             if (número == 3) {
                 var requisição = require("readline-sync");
-                var novocpf = requisição.questionInt("Insira novo cpf sem traços");
+                var novocpf = requisição.question("Insira novo cpf sem traços");
                 ID_unico.cpf = novocpf
             }
             if (número == 4) {
@@ -155,7 +159,7 @@ class sistema{
             var requisição = require("readline-sync");
             var data = requisição.question("Qual sua data de nascimento?\n")
             var requisição = require("readline-sync");
-            var CPF = requisição.questionInt("Qual seu cpf (insira sem traços)?\n");
+            var CPF = requisição.question("Qual seu cpf (insira sem traços)?\n");
             var requisição = require("readline-sync");
             var EMAIL = requisição.question("Qual seu email?\n");
             var requisição = require("readline-sync");
--- teste_entrega.js
+++ teste_entrega.js
@@ -1,1 +1,50 @@
-console.log("HELLO WORLD")
+class Funcionário{
+    constructor(ID_unico,nome_usuario,cpf,email,senha) {
+        this.ID_unico=ID_unico;
+        this.nome_usuario=nome_usuario;
+        this.cpf=cpf;
+        this.email=email;
+        this.senha=senha;
+    }
+    ver_meus_dados() {
+        console.log(this.ID_unico);
+        console.log(this.nome_usuario);
+        console.log(this.cpf);
+        console.log(this.email);
+        console.log(this.senha);
+    }
+    modificar_meus_dados() {
+        var requisição = require("readline-sync");
+        var número = requisição.question("Se desejas alterar, \n seu nome de usuario digite 1 \n seu cpf digite 2 \n seu email digite 3 \n sua senha digite 4 \n");
+        if (número == 1) {
+            var requisição = require("readline-sync");
+            var novonome = requisição.question("Insira novo nome de usuario desejado\n");
+            Funcionário.nome_usuario = novonome
+            console.log("Mudado com sucesso para: ", novonome);
+        }
+        if (número == 2) {
+            var requisição = require("readline-sync");
+            var novocpf = requisição.question("Insira novo cpf sem traços\n");
+            Funcionário.cpf = novocpf
+            console.log("Mudado com sucesso para: ", novocpf);
+        }
+        if (número == 3) {
+            var requisição = require("readline-sync");
+            var novoemail = requisição.question("Insira novo email desejado\n");
+            Funcionário.email = novoemail
+            console.log("Mudado com sucesso para: ", novoemail);
+        }
+        if (número == 4) {
+            var requisição = require("readline-sync");
+            var novasenha = requisição.question("Insira nova senha desejada\n");
+            Funcionário.senha = novasenha
+            console.log("Mudado com sucesso para: ", novasenha);
+        }
+    }
+}
+class Funcionário_logado extends Funcionário {
+    
+}
+
+let Danilo = new Funcionário_logado("78890876","militar_dorminhoco","39749468880","danilo.santiago@gmail.com","54789dan")
+Danilo.modificar_meus_dados();
